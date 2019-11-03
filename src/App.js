09932fb6b1d108720c560b666e8549b8bbaff05a
Martin Klauer <mdklauer@gmail.com>
2019-11-03 18:15:57 +0100

    Fin de la quete react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
+import History from './History';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+          <h1>Hello There ! </h1>
+          <div className='link'>
+            <NavLink exact to="/" activeClassName='red'><p>Home</p></NavLink>
+            <NavLink to="/notre-histoire" activeClassName='red'><p>History</p></NavLink>
+          </div>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/notre-histoire' exact component={History}/>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
